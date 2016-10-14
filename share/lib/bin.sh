6cd566acd777db0ab1ff09bdd5849cdf783dcf7c
Rares Cosma <office@rarescosma.com>
2016-10-14 23:30:47 +0300

    lint bin library

--- share/lib/bin.sh
+++ share/lib/bin.sh
@@ -4,19 +4,20 @@ bin::sync_kube() {
   echo "Checking Kube bins v.${KUBE_VERSION}"
   local target="${DOT}/.bincache/kube_${KUBE_VERSION}" && mkdir -p $target
 
-  if [ ! -x $target/kubectl ]; then
+  if [ ! -x "${target}/kubectl" ]; then
     local tmp=$(mktemp -d)
     local release_dir="${tmp}/kubernetes/server/kubernetes/server/bin"
+    local bin
 
     utils::download $KUBE_TGZ "${tmp}/archive.tgz"
-    tar -C $tmp -xf $tmp/archive.tgz
-    pushd $tmp/kubernetes/server
+    tar -C $tmp -xf "${tmp}/archive.tgz"
+    pushd "${tmp}/kubernetes/server"
     tar xzf kubernetes-server-linux-amd64.tar.gz
     popd
 
-    for X in "${KUBE_BINS[@]}"; do
-      target="${DOT}/.bincache/kube_${KUBE_VERSION}/${X}"
-      [ -x $target ] || mv $release_dir/$X $target
+    for bin in "${KUBE_BINS[@]}"; do
+      target="${DOT}/.bincache/kube_${KUBE_VERSION}/${bin}"
+      [ -x $target ] || mv "${release_dir}/${bin}" $target
     done
   fi
 }
@@ -24,11 +25,11 @@ bin::sync_kube() {
 bin::sync_etcd() {
   echo "Checking Etcd bins v.${ETCD_VERSION}"
   local target="${DOT}/.bincache/etcd_${ETCD_VERSION}" && mkdir -p $target
-  [ -x $target/etcdctl ] || utils::pull_tgz $ETCD_TGZ $target etcd
+  [ -x "${target}/etcdctl" ] || utils::pull_tgz $ETCD_TGZ $target etcd
 }
 
 bin::sync_docker() {
   echo "Checking Docker bins v.${DOCKER_VERSION}"
   local target="${DOT}/.bincache/docker_${DOCKER_VERSION}" && mkdir -p $target
-  [ -x $target/docker ] || utils::pull_tgz $DOCKER_TGZ $target docker
+  [ -x "${target}/docker" ] || utils::pull_tgz $DOCKER_TGZ $target docker
 }

caddfaa4678cc053614c1651f6a17fd286acf709
Rares Cosma <office@rarescosma.com>
2016-10-19 10:09:20 +0200

    Allow non-destructive deployments

--- share/lib/deploy.sh
+++ share/lib/deploy.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
+deploy::apply() {
+  kubectl apply -f "$@"
+}
+
 deploy::loop() {
   kubectl delete -f "$@" 2>/dev/null
   kubectl create -f "$@"
 }
 
-deploy::addon() {
+deploy::apply_addon() {
+  local addon="$1"
+  deploy::apply "${ADDONS_ROOT}/${addon}"
+}
+
+deploy::recreate_addon() {
   local addon="$1"
   deploy::loop "${ADDONS_ROOT}/${addon}"
 }
 
-deploy::app() {
+deploy::apply_app() {
+  local app="$1"
+  deploy::apply "${APPS_ROOT}/${app}"
+}
+
+deploy::recreate_app() {
   local app="$1"
   deploy::loop "${APPS_ROOT}/${app}"
 }

d42dcef45ed8a5c8f8de6849f30ca1ffdc7a35bc
Rares Cosma <office@rarescosma.com>
2016-10-19 10:08:28 +0200

    Remove 'bin' and 'clean' libs. Move bin preparation into 'prepare'

--- share/lib/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-clean::binaries() {
-  rm -rf $DOT/bin/*
-}
-
-clean::tls() {
-  rm -rf $DOT/etc/tls/*
-}
--- share/lib/prepare.sh
+++ share/lib/prepare.sh
@@ -1,10 +1,44 @@
 #!/bin/bash
 
+prepare::bin::sync_kube() {
+  echo "Checking Kube bins v.${KUBE_VERSION}"
+  local target="${DOT}/.bincache/kube_${KUBE_VERSION}" && mkdir -p $target
+
+  if [ ! -x "${target}/kubectl" ]; then
+    local tmp=$(mktemp -d)
+    local release_dir="${tmp}/kubernetes/server/kubernetes/server/bin"
+    local bin
+
+    utils::download $KUBE_TGZ "${tmp}/archive.tgz"
+    tar -C $tmp -xf "${tmp}/archive.tgz"
+    pushd "${tmp}/kubernetes/server"
+    tar xzf kubernetes-server-linux-amd64.tar.gz
+    popd
+
+    for bin in "${KUBE_BINS[@]}"; do
+      target="${DOT}/.bincache/kube_${KUBE_VERSION}/${bin}"
+      [ -x $target ] || mv "${release_dir}/${bin}" $target
+    done
+  fi
+}
+
+prepare::bin::sync_etcd() {
+  echo "Checking Etcd bins v.${ETCD_VERSION}"
+  local target="${DOT}/.bincache/etcd_${ETCD_VERSION}" && mkdir -p $target
+  [ -x "${target}/etcdctl" ] || utils::pull_tgz $ETCD_TGZ $target etcd
+}
+
+prepare::bin::sync_docker() {
+  echo "Checking Docker bins v.${DOCKER_VERSION}"
+  local target="${DOT}/.bincache/docker_${DOCKER_VERSION}" && mkdir -p $target
+  [ -x "${target}/docker" ] || utils::pull_tgz $DOCKER_TGZ $target docker
+}
+
 prepare::bin() {
   mkdir -p $DOT/.bincache $DOT/bin
-  bin::sync_kube &
-  bin::sync_etcd &
-  bin::sync_docker &
+  prepare::bin::sync_kube &
+  prepare::bin::sync_etcd &
+  prepare::bin::sync_docker &
   wait
 
   echo "Creating symlinks"

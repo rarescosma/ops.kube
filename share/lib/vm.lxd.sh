32824f851e52f7ddd8348e099faf4f1cdbf690fa
Rares Cosma <office@rarescosma.com>
2016-10-19 10:07:15 +0200

    Rename lxc to lxd

--- share/lib/vm.lxd.sh
+++ share/lib/vm.lxd.sh
@@ -2,27 +2,27 @@
 
 vm::prepare() {
   vm::update_profile
-  lxc image show $LXC_IMG 2>/dev/null || vm::create_base_image $LXC_IMG
+  lxc image show $LXD_BASE_IMG 2>/dev/null || vm::create_base_image $LXD_BASE_IMG
 }
 
 vm::update_profile() {
-  (lxc profile list | grep $LXC_PROFILE) >/dev/null
+  (lxc profile list | grep $LXD_PROFILE) >/dev/null
   if [ $? -gt 0 ]; then
-    echo "Creating LXC profile ${LXC_PROFILE}"
-    lxc profile create $LXC_PROFILE
+    echo "Creating LXC profile ${LXD_PROFILE}"
+    lxc profile create $LXD_PROFILE
   fi
 
-  echo "Updating ${LXC_PROFILE}"
-  utils::template "${TPL}/lxc_profile.yaml" | lxc profile edit $LXC_PROFILE
+  echo "Updating ${LXD_PROFILE}"
+  utils::template "${TPL}/lxd_profile.yaml" | lxc profile edit $LXD_PROFILE
 }
 
 vm::create_base_image() {
-  local image=${1:-"$LXC_IMG"}
+  local image=${1:-"$LXD_BASE_IMG"}
 
   echo "Creating LXC base image ${image}"
   local vm="kubetmp-$(utils::get_random_string)"
 
-  lxc launch $LXC_IMG_FROM $vm --profile $LXC_PROFILE || lxc start $vm
+  lxc launch $LXD_IMG_FROM $vm --profile $LXD_PROFILE || lxc start $vm
   vm::exec $vm provision::base
 
   lxc stop $vm;
@@ -32,7 +32,7 @@ vm::create_base_image() {
 
 vm::launch() {
   local vm="$1"
-  (lxc launch $LXC_IMG $vm --profile $LXC_PROFILE || lxc start $vm) 2>/dev/null
+  (lxc launch $LXD_BASE_IMG $vm --profile $LXD_PROFILE || lxc start $vm) 2>/dev/null
 }
 
 vm::assert_vm() {
@@ -49,14 +49,14 @@ vm::discover_workers() {
   lxc list -c 4 worker | grep eth0 | cut -d" " -f2
 }
 
-vm::clean_artefacts() {
-  # LXC profile and base image
-  lxc profile delete $LXC_PROFILE
-  lxc image delete $LXC_IMG
+vm::clean() {
+  # Profile and base image
+  lxc profile delete $LXD_PROFILE
+  lxc image delete $LXD_BASE_IMG
 }
 
-vm::delete_containers() {
-  # All containers starting with master or worker
+vm::cluster_down() {
+  # Delete all containers starting with master or worker
   lxc delete -f $(lxc list -c n master | grep master | cut -d" " -f2)
   lxc delete -f $(lxc list -c n worker | grep worker | cut -d" " -f2)
 }

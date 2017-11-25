a2331a1de1d423b480cd0701890822a1c6d3095b
Rareș Cosma <rares@getbetter.ro>
2017-11-25 22:59:36 +0100

    Tweak host preparations on shrewd

--- share/lib/host.shrewd.lxd.sh
+++ share/lib/host.shrewd.lxd.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+host::prepare() {
+  # restart lxd and wait for it
+  sudo killall dnsmasq
+  sudo systemctl restart lxd
+  while true; do
+    lxc list 1>/dev/null && break
+    sleep 1
+  done
+}
+
 host::post() {
   sleep 1
   xmodmap ~/.Xmodmap
@@ -24,7 +34,7 @@ host::resolvconf::stop() {
   sudo chattr -i /etc/resolv.conf
   cat << __EOF__ | sudo tee /etc/resolv.conf
 search lan
-nameserver 192.168.5.5
+nameserver 192.168.0.1
 nameserver 8.8.8.8
 __EOF__
 }

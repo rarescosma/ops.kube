53a0747e3b66046ecce973c56a24ca36442b5922
Rareș Cosma <rares@getbetter.ro>
2018-02-15 22:37:32 +0100

    Use ip route

--- share/lib/network.sh
+++ share/lib/network.sh
@@ -31,20 +31,20 @@ network::start() {
   # Find all worker IPs
   for worker_ip in $(vm::discover worker ips); do
     docker_subnet=$(utils::docker_subnet "$worker_ip")
-    sudo route del -net "$docker_subnet" &>/dev/null || true
-    sudo route add -net "$docker_subnet" gw "$worker_ip"
+    sudo ip route del "$docker_subnet" &>/dev/null || true
+    sudo ip route add "$docker_subnet" via "$worker_ip"
 
     # Proxy services thru all workers
-    sudo route del -net "$KUBE_SERVICE_CLUSTER_IP_RANGE" &>/dev/null || true
-    sudo route add -net "$KUBE_SERVICE_CLUSTER_IP_RANGE" gw "$worker_ip"
+    sudo ip route del "$KUBE_SERVICE_CLUSTER_IP_RANGE" &>/dev/null || true
+    sudo ip route add "$KUBE_SERVICE_CLUSTER_IP_RANGE" via "$worker_ip"
   done
 }
 
 network::stop() {
   dumpstack "$*"
   for subnet in $(seq 1 255); do
-    sudo route del -net "10.${subnet}.0.0/16" &>/dev/null || true &
+    sudo ip route del -net "10.${subnet}.0.0/16" &>/dev/null || true &
   done
   wait
-  sudo route del -net "$KUBE_SERVICE_CLUSTER_IP_RANGE" &>/dev/null || true
+  sudo ip route del -net "$KUBE_SERVICE_CLUSTER_IP_RANGE" &>/dev/null || true
 }

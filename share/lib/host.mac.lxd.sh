9fd1803807049821371e1f43608e6b00463761a8
Rareș Cosma <rares@getbetter.ro>
2018-03-06 11:40:44 +0100

    DNS: - cluster dns + public dns

--- share/lib/host.mac.lxd.sh
+++ share/lib/host.mac.lxd.sh
@@ -21,9 +21,9 @@ host::resolvconf::start() {
   sudo chattr -i /etc/resolv.conf
   cat << __EOF__ | sudo tee /etc/resolv.conf
 search svc.kubernetes.local
-nameserver 10.32.0.10
 nameserver 10.0.40.65
 nameserver 10.10.10.10
+nameserver 8.8.8.8
 __EOF__
   sudo chattr +i /etc/resolv.conf
 }

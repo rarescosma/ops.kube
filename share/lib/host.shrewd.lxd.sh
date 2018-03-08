15b9e379878a6bd5b6df76a785c68442a761e592
Rareș Cosma <rares@getbetter.ro>
2018-03-08 07:57:49 +0100

    [shrewd] restart lxd iff not active

--- share/lib/host.shrewd.lxd.sh
+++ share/lib/host.shrewd.lxd.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
-host::post() {
+host::restart_lxd() {
   dumpstack "$*"
-  echo
+  # restart lxd and wait for it
+  sudo systemctl is-active --quiet lxd || {
+    sudo killall dnsmasq || true
+    sudo systemctl restart lxd
+    while true; do
+      lxc list 1>/dev/null && break
+      sleep 1
+    done
+  }
 }
 
-host::stop() {
+host::prepare() {
   dumpstack "$*"
-  echo
+  host::restart_lxd
+}
+
+host::post() {
+  dumpstack "$*"
 }
 
 host::resolvconf::start() {
@@ -18,10 +30,15 @@ host::resolvconf::start() {
   sudo chattr -i /etc/resolv.conf
   cat << __EOF__ | sudo tee /etc/resolv.conf
 search svc.kubernetes.local
-nameserver 10.0.40.1
+nameserver 10.32.0.10
 nameserver ${dns_ip}
 __EOF__
   sudo chattr +i /etc/resolv.conf
+}
+
+host::stop() {
+  dumpstack "$*"
+  host::resolvconf::stop
 }
 
 host::resolvconf::stop() {

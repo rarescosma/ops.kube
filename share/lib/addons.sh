bd0e2362a23a2416ed854d8a4cb23bd317e3eeca
Rareș Cosma <rares@getbetter.ro>
2018-03-11 13:17:43 +0100

    Addon metallb + federation support

--- share/lib/addons.sh
+++ share/lib/addons.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
 
-addon::coredns() {
+addon() {
+  local addon
+  addon="${1}"
   export KUBE_DNS_IP="$(utils::service_ip "$KUBE_SERVICE_CLUSTER_IP_RANGE")00"
   export KUBE_DNS_DOMAIN="kubernetes.local"
 
-  utils::template "$TPL/addons/coredns.yaml.tpl" \
-  | sed 's/: true/: "true"/g' \
-  | tee "$TPL/addons/coredns.yaml" \
+  utils::template "$TPL/addons/${addon}.yaml.tpl" \
+  | tee "$TPL/addons/${addon}.yaml" \
   | kubectl apply -f -
+}
+
+addon::federation() {
+  addon "federation"
+  helm init --service-account="tiller" --tiller-namespace="kube-system" --upgrade || true
+
+  helm del --purge etcd-operator
+
+  helm ls --all etcd-operator | grep DEPLOYED || {
+    helm install --namespace federation-support --name etcd-operator stable/etcd-operator
+    helm upgrade --namespace federation-support --set cluster.enabled=true etcd-operator stable/etcd-operator
+  }
+
+  helm del --purge coredns
+
+  helm ls --all coredns | grep DEPLOYED || {
+    helm install --namespace federation-support --name coredns -f "${TPL}/addons/FederationCore.yaml" stable/coredns
+  }
 }
---
+++ share/templates/addons/federation.yaml.tpl
@@ -0,0 +1,40 @@
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: federation-support
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  annotations:
+    rbac.authorization.kubernetes.io/autoupdate: "true"
+  labels:
+    kubernetes.io/bootstrapping: rbac-defaults
+  name: federation-support:admin
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: admin
+subjects:
+- kind: ServiceAccount
+  name: default
+  namespace: federation-support
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: tiller
+  namespace: kube-system
+---
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRoleBinding
+metadata:
+  name: tiller
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+  - kind: ServiceAccount
+    name: tiller
+    namespace: kube-system
